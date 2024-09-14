233592c7e8231f8f8e93b426e5f4415cd39e6f62
JonathanManaka <mphomanaka91@gmail.com>
2024-09-14 15:59:07 +0200

    rectified the routes names and added new route to auth-customer

--- route/customerRoutes.js
+++ route/customerRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { createCustomer, getCustomer } = require('../controllers/customerController');
+const { createCustomer, getCustomer, authenticateCustomer } = require('../controllers/customerController');
 
 const router = express.Router();
 
-router.post('/customer', createCustomer);  
-router.get('/customer/:custID_Nr', getCustomer);  
+router.post('/register_customer/', createCustomer);  
+router.get('/get_customer/:custID_Nr', getCustomer); 
+router.get('/auth_customer/', authenticateCustomer);  
 
 module.exports = router;
