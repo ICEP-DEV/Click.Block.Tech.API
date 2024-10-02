ced27a913b91763c3ff2f56271be8fa3fb19e4e9
JonathanManaka <mpomanaka91@gmail.com>
2024-10-02 15:29:20 +0200

    changed route to include accNr and Loginpin params

--- route/customerRoutes.js
+++ route/customerRoutes.js
@@ -8,6 +8,6 @@ router.patch('/customers/:custID_Nr', updateCustomerStep);
 router.get('/get_customer/:custID_Nr', getCustomer);
 router.post('/customers/verify-otp', verifyOtp);
 router.get('/get_accountNr/:AccountNr', getAccountID);
-router.get('/get_customer_byID/:AccountNr', getCustomerByAccNr);
+router.get('/get_customer_byID/:AccountNr/:LoginPin/', getCustomerByAccNr);
 
 module.exports = router;
