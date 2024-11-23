3b987530faef4de3adbf565a4797d44583bed679
JonathanManaka <mpomanaka91@gmail.com>
2024-11-23 10:26:14 +0200

    added two routes, updateLatLong and getAlertLocationID

--- route/alertRoutes.js
+++ route/alertRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const {createLocation, createAlert} = require('../controllers/alertController');
+const {createLocation, createAlert,updateLatLong,getAlertLocationID} = require('../controllers/alertController');
 const router = express.Router();
 
 router.post('/createLocation', createLocation);
 router.post('/createAlert', createAlert);
+router.put('/updateLatLong/:locationID',updateLatLong);
+router.get('/getAlertLocationID/:custID_Nr',getAlertLocationID)
 
 module.exports = router;
