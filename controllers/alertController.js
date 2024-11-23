725dd5ced63d2afd2f59b3eb0122a2c7b4e9958d
JonathanManaka <mpomanaka91@gmail.com>
2024-11-23 10:23:55 +0200

    created update lat and long controller

--- controllers/alertController.js
+++ controllers/alertController.js
@@ -22,4 +22,30 @@ const createAlert = (req, res)=>{
         res.status(201).json(result);
     });
 }
-module.exports = {createLocation, createAlert};
+const updateLatLong = (req, res)=>{
+    const locationData = req.body;
+    const locationId = req.params.locationID
+    AlertService.updateLatLong(locationId, locationData, (err, result)=>{
+        if(err){
+            console.log('Error updating Lat Long', err);
+            return res.status(500).json({error: 'Failed to update Lat Long', message: err.message});
+        }
+        res.status(201).json(result);
+    });
+
+}
+const getAlertLocationID = (req,res)=>{
+    const custID_Nr = req.params.custID_Nr;
+      AlertService.getAlertLocationID(custID_Nr,(err, result) => {
+        if(err){
+          return res.status(500).send(err);
+        }
+        if(result){
+          res.status(200).send(result);
+        }else{
+          res.status(404).send('Alert Location ID  not found).');
+        }
+      });
+    };
+
+module.exports = {createLocation, createAlert, updateLatLong, getAlertLocationID};
