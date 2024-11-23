767db490f03c8e7cbba53fd2b0f45997d80188a7
Nchavelelo <NchaveleloSithole@gmail.com>
2024-11-23 17:47:54 +0200

    Adjusted AlertPinLogs

--- route/alertPinLogRoutes.js
+++ route/alertPinLogRoutes.js
@@ -1,16 +1,9 @@
-// routes/alertPinLogRoutes.js
-
 const express = require('express');
+const { logAlertPin, getAllAlertPinLogs } = require('../controllers/alertPinLogController');
+
 const router = express.Router();
-const AlertPinLogController = require('../controllers/alertPinLogController');
 
-// Endpoint to get AlertPin usage count by month and year
-router.get('/:custID/monthlyUsage', AlertPinLogController.countAlertPinUsageByMonth);
-
-// Endpoint to get total AlertPin usage count
-router.get('/:custID/totalUsage', AlertPinLogController.countUsageByCustomerId);
-
-// Endpoint to get AlertPin logs for all customers
-router.get('/allCustomers/logs', AlertPinLogController.getAllCustomerAlertPinLogs);
+router.post('/logAlertPin', logAlertPin); // Logs a new pin usage
+router.get('/alertPinLogs', getAllAlertPinLogs); // Retrieves all logs
 
 module.exports = router;
