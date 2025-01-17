ebfb8dbc92215b47d495fd105106839a55baf34e
JonathanManaka <mpomanaka91@gmail.com>
2025-01-17 09:27:00 +0200

    created bot using gemini generative ai

--- controllers/contactMeMessageController.js
+++ controllers/contactMeMessageController.js
@@ -1,3 +1,4 @@
+
 const ContactMeMessageService = require('../service/contactMeMessageService');
 
 const createMessage = (req, res) => {
@@ -8,7 +9,19 @@ const createMessage = (req, res) => {
   });
 };
 
+const getChatBotResponse  = async (req, res)  =>{
+  const prompt = req.body;
+  const { GoogleGenerativeAI } = require("@google/generative-ai");
 
+  const genAI = new GoogleGenerativeAI("AIzaSyDODFo_ijTlgWCxtqebqvEEcvT6jfBQYmM");
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+  const result = await model.generateContent(
+    `**prompt:** **User Question:**  {${prompt.prompt}}\n\n**App Description:** Our app enhances your banking app's security by implementing a two-pin system to combat kidnapping-related financial threats prevalent in South Africa. \n* **Emergency Pin:** Used to lock your account and alert authorities during a kidnapping. \n* **Regular Pin:** For everyday transactions. \n\n**Features:**\n* **Kidnap Protection:** Prevents unauthorized fund transfers during emergencies. \n* **Transaction Safety:** Enhances security for all transactions within the banking app. \n\n**Pricing:** Our platform is currently **free** to use. \n\n**Registration:** Requires basic personal information and image authentication. \n\n**Panic Feature Activation:** Requires setting a unique panic pin once. \n\n**User Question:** {${prompt.prompt}}\n\n**Answer:** Based on the provided app description and user question, please provide a comprehensive and informative response. If the user's question requires technical support or troubleshooting, kindly advise them to contact our admin team.**Create a short answer with paragraph of 4 lines`,
+  );
+  res.status(201).json({response: `${result.response.text()}`})
+}
+
+ 
 const getMessage = (req, res) => {
   const MessageID = req.params.messageID;
   ContactMeMessageService.getMessageById(MessageID, (err, result) => {
@@ -52,5 +65,6 @@ module.exports = {
   getMessage,
   updateMessageStatus,
   deleteMessage,
-  getAllMessages
+  getAllMessages,
+  getChatBotResponse
 };
